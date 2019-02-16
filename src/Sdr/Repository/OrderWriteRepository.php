bec85c568e4e59447f0ddf2ee6bdfab008bc9d71
Chris Armitage <dev@chrisarmitage.com>
2019-02-16 23:32:20 +0000

    Change Write Repository to return Order domain object

--- src/Sdr/Repository/OrderWriteRepository.php
+++ src/Sdr/Repository/OrderWriteRepository.php
@@ -2,6 +2,8 @@
 
 namespace Sdr\Repository;
 
+use Sdr\Domain\Order;
+
 class OrderWriteRepository
 {
     /**
@@ -18,9 +20,34 @@ class OrderWriteRepository
         $this->pdo = $pdo;
     }
 
-    public function update(array $order) : void
+    /**
+     * @param $orderId
+     * @return Order
+     */
+    public function get($orderId) : Order
+    {
+        $sth = $this->pdo->prepare('SELECT * FROM orders WHERE id = :id LIMIT 1');
+        $sth->execute(['id' => $orderId]);
+        $row = $sth->fetch(\PDO::FETCH_ASSOC);
+
+        $order = new Order(
+            $row['id'],
+            null,
+            $row['state'],
+            0
+        );
+
+        return $order;
+    }
+
+    public function update(Order $order) : void
     {
         $sth = $this->pdo->prepare('UPDATE orders SET state = :state WHERE id = :id');
-        $sth->execute(['id' => $order['id'], 'state' => $order['state']]);
+        $sth->execute(
+            [
+                'id' => $order->getId(),
+                'state' => $order->getState(),
+            ]
+        );
     }
 }
